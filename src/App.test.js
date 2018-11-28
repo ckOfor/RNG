cd795e86b3fa5ad772cce47b67e795691458d44b
Ofor Chinedu <chinedu@nomadinteractive.co>
2018-11-28 02:01:35 +0100

    add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
+// react
 import React from 'react';
+
+// third party
 import ReactDOM from 'react-dom';
-import App from './App';
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+// component
+import App from './components/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('<MyComponent />', () => {
+  it('renders three <App /> components', () => {
+    const wrapper = shallow(<App />);
+    // expect(wrapper.find(Foo)).to.have.lengthOf(3);
+  });
 });
